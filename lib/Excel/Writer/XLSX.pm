8d8fcb78172e2e1c1de62100d9ae694a534ec53d
John McNamara <jmcnamara@cpan.org>
2012-12-31 13:35:23 +0000

    Import of JMCNAMARA/Excel-Writer-XLSX-0.65 from CPAN.
    gitpan-cpan-distribution: Excel-Writer-XLSX
    gitpan-cpan-version:      0.65
    gitpan-cpan-path:         JMCNAMARA/Excel-Writer-XLSX-0.65.tar.gz
    gitpan-cpan-author:       JMCNAMARA
    gitpan-cpan-maturity:     released

--- lib/Excel/Writer/XLSX.pm
+++ lib/Excel/Writer/XLSX.pm
@@ -18,7 +18,7 @@ use strict;
 use Excel::Writer::XLSX::Workbook;
 
 our @ISA     = qw(Excel::Writer::XLSX::Workbook Exporter);
-our $VERSION = '0.64';
+our $VERSION = '0.65';
 
 
 ###############################################################################
@@ -50,7 +50,7 @@ Excel::Writer::XLSX - Create a new file in the Excel 2007+ XLSX format.
 
 =head1 VERSION
 
-This document refers to version 0.64 of Excel::Writer::XLSX, released December 22, 2012.
+This document refers to version 0.65 of Excel::Writer::XLSX, released December 31, 2012.
 
 
 
@@ -872,7 +872,7 @@ String fragments that don't have a format are given a default format. So for exa
     $worksheet->write_rich_string( 'A1',
         $default, 'Some ', $bold, 'bold', $default, ' text' );
 
-As with Excel, only the font properties of the format such as font name, style, size, underline, color and effects are applied to the string fragments. Other features such as border, background and alignment must be applied to the cell.
+As with Excel, only the font properties of the format such as font name, style, size, underline, color and effects are applied to the string fragments. Other features such as border, background, text wrap and alignment must be applied to the cell.
 
 The C<write_rich_string()> method allows you to do this by using the last argument as a cell format (if it is a format object). The following example centers a rich string in the cell:
 
